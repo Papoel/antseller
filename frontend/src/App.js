70c4be438ce9fa3fdf112e98469559577a5ced12
Papoel <pascal.briffard@gmail.com>
2022-03-16 01:11:42 +0100

    Add style Navbar and Add Footer whithout style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { Container } from 'react-bootstrap'
+import Header from './compopnents/Header'
+import Footer from './compopnents/Footer'
+
 const App = () => {
   return (
     <>
-      <h1>Bienvenu sur AntSeller !</h1>
+        <Header />
+        <main className='py-3'>
+            <Container>
+                <h1>Bienvenu sur AntSeller !</h1>
+            </Container>
+        </main>
+        <Footer />
     </>
   );
 }
