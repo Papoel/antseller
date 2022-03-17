dda0e947220707361d79abe4b5046338010d66e6
Papoel <pascal.briffard@gmail.com>
2022-03-18 00:51:53 +0100

    Fix Bug : Await was missing for bcrypt using

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,18 +4,18 @@ const users = [
     {
         name: 'Pascal Briffard',
         email: 'admin@email.fr',
-        password: bcrypt.hash('password', 10),
+        password: await bcrypt.hash('password', 10),
         isAdmin: true,
     },
     {
         name: 'Christopher Gosselin',
         email: 'user0@email.fr',
-        password: bcrypt.hash('password', 10),
+        password: await bcrypt.hash('password', 10),
     },
     {
         name: 'Luc skywalker',
         email: 'user1@email.fr',
-        password: bcrypt.hash('password', 10),
+        password: await bcrypt.hash('password', 10),
     },
 ]
 
