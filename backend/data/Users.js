dc299d5036291e6f644d00aa3f08f75b7633d3ee
Papoel <pascal.briffard@gmail.com>
2022-03-17 21:19:22 +0100

    Create 3 Users

---
+++ backend/data/Users.js
@@ -0,0 +1,18 @@
+const user = [
+    {
+        name: 'Pascal Briffard',
+        email: 'admin@email.fr',
+        password: 'xxxx->HASH',
+        isAdmin: true,
+    },
+    {
+        name: 'Christopher Gosselin',
+        email: 'user0@email.fr',
+        password: 'xxxx->HASH',
+    },
+    {
+        name: 'Luc skywalker',
+        email: 'user1@email.fr',
+        password: 'xxxx->HASH',
+    },
+]
