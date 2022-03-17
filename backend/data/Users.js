6c74cdab9185e5592f009ed9a8323fa6e120a897
Papoel <pascal.briffard@gmail.com>
2022-03-17 21:27:08 +0100

    Add bcryptjs and Hash Password

--- backend/data/Users.js
+++ backend/data/Users.js
@@ -1,18 +1,22 @@
-const user = [
+import bcrypt from 'bcryptjs'
+
+const users = [
     {
         name: 'Pascal Briffard',
         email: 'admin@email.fr',
-        password: 'xxxx->HASH',
+        password: bcrypt.hash('password', 10),
         isAdmin: true,
     },
     {
         name: 'Christopher Gosselin',
         email: 'user0@email.fr',
-        password: 'xxxx->HASH',
+        password: bcrypt.hash('password', 10),
     },
     {
         name: 'Luc skywalker',
         email: 'user1@email.fr',
-        password: 'xxxx->HASH',
+        password: bcrypt.hash('password', 10),
     },
 ]
+
+export default users
