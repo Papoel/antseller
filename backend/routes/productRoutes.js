401b1b9fa7a1d90fcb90fdc07bc06028a7bd6364
Papoel <pascal.briffard@gmail.com>
2022-03-22 22:27:35 +0100

    Add Routes for Product and configuration of server for API REST Standar using

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import colors from 'colors'
+import asyncHandler from 'express-async-handler'
+import 'express-async-errors'
+import Product from '../models/productModel.js'
+
+const router = express.Router()
+
+/*
+ * @description:    Récupère tous les produits
+ * @route:          GET api/products
+ * @access:         Public
+*/
+router.get('/', asyncHandler(async(request, response) => {
+    try {
+        const products = await Product.find({})
+        response.json(products)
+    } catch (error) {
+        console.log("Une erreur s'est produite".white.bgRed)
+    }
+}))
+
+/*
+ * @description:    Récupère un produit
+ * @route:          GET api/product/:id
+ * @access:         Public
+*/
+router.get('/:id',asyncHandler( async (request, response) => {
+    const product = await Product.findById(request.params.id)
+    if (product) {
+        response.json(product)
+    } else {
+        response.status(404).json({ message: "Cet article n'existe pas !" })
+    }
+}))
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -4,35 +4,47 @@ import colors from 'colors'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import cors from 'cors'
 
 dotenv.config()
-
-// connectDB()
+// Se connecter à la base de donnée.
+connectDB()
 
 const app = express()
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods',
-        'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+// Paramétrer les Headers
+app.use((request, response, next) => {
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    )
+    response.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    )
+    next()
+})
 
-app.use(express.json)
 
-app.get('/', (req, res) => {
-    res.write('Avec la fonction write : API est en cours d\'éxécution')
-    res.end()
-    })
+
+app.use(express.json())
+
+app.use(cors())
 
 app.use('/api/products', productRoutes)
 
-const PORT = process.env.PORT || 5000
+app.get('/', (request, response) => {
+    response.send("API est en cours d'éxécution")
+})
+
+
+const PORT = process.env.PORT || 8080
 const ENV = process.env.NODE_ENV
 
-app.listen(
-    PORT,
-    'localhost',
-    console.log(`Le serveur AntSeller tourne en mode ${ENV} sur le port : ${PORT}`.cyan.italic.bold))
+// noinspection JSForgottenDebugStatementInspection,JSCheckFunctionSignatures,JSVoidFunctionReturnValueUsed
+app.listen(PORT, console.log(
+    `Le serveur AntSeller tourne en mode ${ENV} sur http://localhost:${PORT}`
+        .cyan.italic.bold
+    )
+)
