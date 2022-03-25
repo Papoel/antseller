e101fe03177216433d30445831e7a76e1c552cca
Papoel <pascal.briffard@gmail.com>
2022-03-25 14:11:18 +0100

    Add Remove From Cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { useDispatch,  useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card, ListGroupItem } from 'react-bootstrap'
 import Message from '../compopnents/Message'
-import { addToCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
 
 const CartScreen = ({ match, location, history }) => {
     const productId = match.params.id
@@ -22,7 +22,9 @@ const CartScreen = ({ match, location, history }) => {
         }
     }, [dispatch, productId, quantity])
 
-    const removeFromCartHandler = (id) => console.log('remove')
+    const removeFromCartHandler = (id) => {
+        dispatch(removeFromCart(id))
+    }
 
     const checkoutHandler = () => {
         // Todo => Attention à l'URL !
@@ -31,76 +33,85 @@ const CartScreen = ({ match, location, history }) => {
 
     return <Row>
         <Col md={8}>
-        <h1>panier d'achat</h1>
-        {cartItems.length === 0 ? (
-            <Message>
-                Votre panier est vide
-                    <Link className='btn btn-outline-info mx-1 py-0 px-1 rounded' to='/'>revenir à la liste de produits</Link>
-                en vente.
-            </Message> 
-            ) : (
-            <ListGroup variant='flush'>
-                {cartItems.map(item => (
-                    <ListGroup.Item key={ item.product }>
-                        <Row>
-                            <Col md={2}>
-                                < Image src={ item.image } alt={item.name} fluid rounded />
-                            </Col>
-                            <Col md={3}>
-                                <Link to={`/product/${item.product}`}>{item.name}</Link>
-                            </Col>
-                            <Col md={2}> {item.price}€ </Col>
-                            <Col md={2}>
+            <h1>panier d'achat</h1>
+            {cartItems.length === 0 ? (
+                <Message>
+                    Votre panier est vide
+                        <Link className='btn btn-outline-info mx-1 py-0 px-1 rounded' to='/'>revenir à la liste de produits</Link>
+                    en vente.
+                </Message> 
+                ) : (
+                <ListGroup variant='flush'>
+                    {cartItems.map(item => (
+                        <ListGroup.Item key={ item.product }>
+                            <Row>
+                                <Col md={2}>
+                                    < Image src={ item.image } alt={item.name} fluid rounded />
+                                </Col>
+                                <Col md={3}>
+                                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                </Col>
+                                <Col md={2}> {item.price}€ </Col>
+                                <Col md={2}>
                                 <Form.Control
-                                    as = 'select'
-                                    value = { item.quantity }
-                                    onChange = { (e) => 
+                                    as='select'
+                                    value={item.qty}
+                                    onChange={(e) =>
                                         dispatch(
-                                            addToCart(item.product, Number(e.target.value))
+                                        addToCart(item.product, Number(e.target.value))
                                         )
                                     }
-                                >
+                                    >
                                     {[...Array(item.countInStock).keys()].map((x) => (
-                                        <option key = {x + 1} value = {x + 1}> 
-                                            {x + 1} 
+                                        <option key={x + 1} value={x + 1}>
+                                        {x + 1}
                                         </option>
                                     ))}
                                 </Form.Control>
-                            </Col>
-                            <Col md={2}>
-                                <Button 
-                                    type='button' 
-                                    variant='light' 
-                                    onClick={ () => removeFromCartHandler(item.product) }>
-                                    <i className='fas fa-trash text-danger'></i>
-                                </Button>
-                            </Col>
-                        </Row>
-                    </ListGroup.Item>
-                ))}
-            </ListGroup>
-        ) }
+                                </Col>
+                                <Col md={2}>
+                                    <Button 
+                                        type='button' 
+                                        variant='light' 
+                                        onClick={() => removeFromCartHandler(item.product)}>
+                                        <i className='fas fa-trash text-danger'></i>
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+            ) }
         </Col>
         <Col md={4}>
             <Card>
                 <ListGroup variant='flush'>
                     <ListGroupItem>
                         <h2 className='text-center' >
-                            ({ cartItems.reduce((acc, item) => acc + item.quantity, 0 ) }) articles
+                            { cartItems.reduce((acc, item) => acc + item.quantity, 0 ) } article(s)
                         </h2>
-                        { cartItems
-                            .reduce((acc, item) => acc + item.quantity * item.price, 0)
-                            .toFixed(2) 
-                        }€
+                        {cartItems.length > 0 ? (
+                            <p>
+                                <span> Sous-Total :</span>
+                                <span className='p-1 fw-bold'>
+                                    { cartItems
+                                        .reduce((acc, item) => acc + item.quantity * item.price, 0)
+                                        .toFixed(2) 
+                                    }€
+                                </span>
+                            </p>
+                        ) : ( ''
+                        )}
+                        
                     </ListGroupItem>
                     <ListGroupItem>
                         <Button 
                             type='button' 
-                            className='btn btn-block w-100' 
+                            className='btn btn-success btn-block w-100' 
                             disabled={cartItems.length === 0} 
                             onClick={checkoutHandler}
                         >
-                            procéder au paiement
+                            <span className='font-weight-bold h5'> procéder au paiement </span>
                         </Button>
                     </ListGroupItem>
                 </ListGroup>
